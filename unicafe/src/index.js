e08b5d27e1a055b3b8846f295cb319d9195d317c
ivoryblakk <george.ingram1@outlook.com>
2019-07-08 11:02:56 -0400

    unicafe step2

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -10,40 +10,38 @@ const App = () => {
   const Feedback = () =>{
     // TODO header Give  Feedback 
 
-
     return (
         <>
         <h1>  Give Feedback </h1>
         <br/>
-        <FeedbackBtn name="good" setFeedback= {setGood} feedback = {good} />
-        <FeedbackBtn name="neutral" setFeedback= {setNeutral} feedback = {neutral}  />
-        <FeedbackBtn name="bad" setFeedback= {setBad} feedback = {bad}/>
+        <FeedbackBtn name="good" value={1} setFeedback= {setGood} feedback = {good} />
+        <FeedbackBtn name="neutral" value={0} setFeedback= {setNeutral} feedback = {neutral}  />
+        <FeedbackBtn name="bad" value={-1} setFeedback= {setBad} feedback = {bad}/>
         </>
     )
   };
   //Reusable button 
-  const FeedbackBtn = ({name, feedback, setFeedback}) => {
+  
+  const FeedbackBtn = ({name, value,  feedback, setFeedback}) => {
     return <button  onClick={()=>{ setFeedback(feedback +1 )}}> {name}</button>;
   }
 
   const Statistics =() => {
-
+      const all = good+neutral+bad; 
+      const averages = ((1*good) +(-1*bad) )/all;
     return (
         <>
         <span> <p> good {good}</p></span>
         <span> <p> neutral {neutral}</p></span>
         <span> <p> bad {bad}</p></span>
+        <span> <p> all {all}</p></span>
+        <span> <p> average {averages}</p></span>
+        <span> <p> positive {good/all * 100} %</p></span>
         </>
     )
 
   }
-  //Add function
 
-  //Average
-
-  //
-
- 
 
   return (
     <div>
