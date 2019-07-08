6a22385c4d89fd6b57627c19dbccc1cbb97f1fed
ivoryblakk <george.ingram1@outlook.com>
2019-07-08 11:51:08 -0400

    unicafe steps 3-5

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -8,7 +8,6 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const Feedback = () =>{
-    // TODO header Give  Feedback 
 
     return (
         <>
@@ -22,32 +21,39 @@ const App = () => {
   };
   //Reusable button 
   
-  const FeedbackBtn = ({name, value,  feedback, setFeedback}) => {
+  const FeedbackBtn = ({name, feedback, setFeedback}) => {
     return <button  onClick={()=>{ setFeedback(feedback +1 )}}> {name}</button>;
   }
 
-  const Statistics =() => {
+  const Statistic = (props) =>{
+      const {text, value} = props;
+      return <tr> <td> {text} </td> <td> {value} {text == 'positive'? "%": "" } </td> </tr> ;
+  }
+
+  const Statistics =(props) => {
       const all = good+neutral+bad; 
       const averages = ((1*good) +(-1*bad) )/all;
     return (
         <>
-        <span> <p> good {good}</p></span>
-        <span> <p> neutral {neutral}</p></span>
-        <span> <p> bad {bad}</p></span>
-        <span> <p> all {all}</p></span>
-        <span> <p> average {averages}</p></span>
-        <span> <p> positive {good/all * 100} %</p></span>
+        <h1>Statistics </h1>
+        <table>
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={all} />
+        <Statistic text="average" value={averages.toFixed(1)} />
+        <Statistic text="positive" value={(good/all * 100).toFixed(1)} />
+        </table>
         </>
     )
 
   }
 
-
   return (
     <div>
       <Feedback />
       <br/>
-      <Statistics/>
+      {good == 0 && neutral == 0 && bad == 0 ? " No feedback given" : <Statistics/>}
     </div>
   )
 };
